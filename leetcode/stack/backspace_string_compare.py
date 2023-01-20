277418e66bc9136aa335f95ed0166e625e6fde97
Konrad Droeske <kdroeske@gmail.com>
2023-01-20 10:36:54 -0500

    used reversed pointers for backspace string compare

---
+++ leetcode/stack/backspace_string_compare.py
@@ -0,0 +1,67 @@
+def backspace_compare(s: str, t: str) -> bool:
+    stack_s = []
+    stack_t = []
+
+    for char in s:
+        if char != "#":
+            stack_s.append(char)
+        elif stack_s:
+            stack_s.pop()
+
+    for char in t:
+        if char != "#":
+            stack_t.append(char)
+        elif stack_t:
+            stack_t.pop()
+
+    return stack_s == stack_t
+
+
+def backspace_compare_optimized(s: str, t: str) -> bool:
+    counter_s = 0
+    counter_t = 0
+
+    pointer_s = len(s) - 1
+    pointer_t = len(t) - 1
+
+    while pointer_s >= 0 or pointer_t >= 0:
+        char_s = s[pointer_s] if pointer_s >= 0 else ""
+        char_t = t[pointer_t] if pointer_t >= 0 else ""
+
+        if char_s == "#":
+            counter_s += 1
+            pointer_s -= 1
+            continue
+
+        if char_t == "#":
+            counter_t += 1
+            pointer_t -= 1
+            continue
+
+        if counter_s > 0:
+            counter_s -= 1
+            pointer_s -= 1
+            continue
+
+        if counter_t > 0:
+            counter_t -= 1
+            pointer_t -= 1
+            continue
+
+        # compare values
+        if char_s != char_t:
+            return False
+
+        pointer_s -= 1
+        pointer_t -= 1
+
+    return True
+
+
+print(backspace_compare_optimized("ab#c", "ad#c"))  # true
+print(backspace_compare_optimized("ab##", "c#d#"))  # true
+print(backspace_compare_optimized("a#c", "b"))  # false
+print(backspace_compare_optimized("y#fo##f", "y#f#o##f"))  # true
+
+print(backspace_compare_optimized("bxj##tw", "bxj###tw"))  # false
+print(backspace_compare_optimized("hd#dp#czsp#####", "hd#dp#czsp######"))  # true
